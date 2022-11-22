c1171d0504f1a6ae8bd729da8ed55a5ab6fb3034
Khashayar Haghighi <xerxes.2000a@gmail.com>
2022-11-22 19:41:38 +0330

    features added

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ function update(){
     let rnd1 = Math.floor(Math.random() * 6) + 1
 let rnd2 = Math.floor(Math.random() * 6) + 1
 
-document.getElementById('dice1').setAttribute('src', `./images/dice${rnd1}.png`)  
-document.getElementById('dice2').setAttribute('src', `./images/dice${rnd2}.png`) 
+dice1 = document.getElementById('dice1')
+dice2 = document.getElementById('dice2')
+
+toggleSequence("dice1")
+dice1.setAttribute('src', `./images/dice${rnd1}.png`)
+
+dice2.setAttribute('src', `./images/dice${rnd2}.png`)
+toggleSequence("dice2")
+
 let winner;
-if(rnd1 > rnd2) {winner = `Player 1 is Winner !`} else if (rnd1 === rnd2) {winner = "It's a match!"} else {winner = `Player 2 is Winner !`}
+if(rnd1 > rnd2) 
+    {winner = `Player 1 is Winner !`} 
+else if (rnd1 === rnd2) 
+    {winner = "It's a match!"} 
+else 
+    {winner = `Player 2 is Winner !`}
+
 document.getElementById('winner-text').textContent = winner
 }
 
+function toggleSequence(dice) {
+    setTimeout(() => {
+        document.getElementById(dice).classList.toggle("shake");
+    }, 0)
+    setTimeout(() => {
+        document.getElementById(dice).classList.toggle("shake");
+    }, 820)
+  }
 
 document.getElementById('button').addEventListener('click', update)
